ae7871c629fbf796ec25c3efaa0896377e611134
Peter Hanson <peter@upandrunningsoftware.com>
2011-02-12 23:24:22 -0600

    updated config file

--- config.php
+++ config.php
@@ -6,8 +6,8 @@ class config
     public static $dbClassFile = "pgsql_database.php";
     public static $dbHost = "localhost";
     public static $dbName = "test";
-    public static $dbUsername = "peteh";
-    public static $dbPassword = "save99on";
+    public static $dbUsername = "root";
+    public static $dbPassword = "root";
 
 }
 
