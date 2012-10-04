cca47ec14bf79f726f523834823b4ba1387b558b
Faris Zacina <faris.zacina@upandrunningsoftware.com>
2012-10-04 19:30:00 +0200

    Ticket #13 - testing larger dump files

--- config.php
+++ config.php
@@ -22,14 +22,14 @@ class config
      * @var array
      */
     public static $db = array(
-         'blog' => array(
+        /* 'blog' => array(
             'driver' => 'mysql_database.php',
             'host'   => 'localhost',
             'name'   => 'my_blog',
-            'user'   => 'nonadmin',
-            'pass'   => 'nonadmin',
+            'user'   => 'cake',
+            'pass'   => 'letthemeat',
 	    'track_patches_in_file'   => false,
-        ), /*
+        ), /* */
         'cms' => array(
             'driver' => 'mysql_database.php',
             'host'   => 'localhost',
