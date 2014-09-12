2ed526068023b6311b3ab4940bcf46f0e8285a37
Peter Hanson <peter@upandrunningsoftware.com>
2014-09-12 12:52:16 -0500

    removed config.php from gitignore

---
+++ installations/composer/db.php
@@ -0,0 +1,7 @@
+<?php
+require_once(dirname(__FILE__) . '/../vendor/uarsoftware/dbpatch/DbPatch/Config/Master.php');
+
+class db extends DbPatch_Config_Master
+{
+
+}
---
+++ installations/git_submodule/patch_database.php
@@ -0,0 +1,5 @@
+<?php
+// make DBPATCH_BASE_PATH local to here
+define('DBPATCH_BASE_PATH', dirname(__FILE__));
+
+require_once(dirname(__FILE__) . '/../dbpatch/patch_database.php');
