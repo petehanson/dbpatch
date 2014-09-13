7c4c9d1b6d9167eaeda0f517dd2fd9cd940bd639
Peter Hanson <peter@upandrunningsoftware.com>
2014-09-13 11:05:49 -0500

    initial command build, based on symfonys console tool

---
+++ src/Command/PatchCommand.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace uarsoftware\dbpatch\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class PatchCommand extends Command {
+
+    protected function configure() {
+        $this->setName("test");
+        $this->setDescription("A test command");
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $output->writeln("Hello World");
+    }
+}
