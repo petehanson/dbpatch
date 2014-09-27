8fb6f5b32f7c900533758ac7af971b811044edb0
Peter Hanson <peter@upandrunningsoftware.com>
2014-09-27 10:51:49 -0500

    deleted the installations folder, since this would only be installed by composer now

--- installations/composer/patch_database.php
+++
@@ -1,5 +0,0 @@
-<?php
-// make DBPATCH_BASE_PATH local to here
-define('DBPATCH_BASE_PATH', dirname(__FILE__));
-
-require_once(dirname(__FILE__) . '/../vendor/uarsoftware/dbpatch/patch_database.php');
--- installations/git_submodule/reset_db_wrapper.php
+++
@@ -1,9 +0,0 @@
-<?php
-// add a config.php and db.php here locally
-// in this folder
-
-// make DBPATCH_BASE_PATH local to here
-define('DBPATCH_BASE_PATH', dirname(__FILE__));
-
-// now get the database patcher
-require_once(dirname(__FILE__) . '/../dbpatch/reset_db.php');
